ba3a9633baf3483ea758db008523a95a329a504d
dhrubosabbir <heartedsabbirdhrubo@gmail.com>
2022-01-04 01:54:56 +0600

    upload

---
+++ app.js
@@ -0,0 +1,202 @@
+// simpleFormValidaion
+const simpleForm = document.querySelector("#simpleForm")
+const name = document.querySelector("#name")
+const email = document.querySelector("#email")
+const password = document.querySelector("#password")
+const phone = document.querySelector("#phone")
+const city = document.querySelector("#city")
+const zipCode = document.querySelector("#zip-code")
+const button = document.querySelector("#button")
+const label = document.querySelector(".input-box label")
+const input_box = document.querySelectorAll(".input-box")
+const p = document.querySelector(".input-box p")
+const alrt = document.getElementById('alrt')
+
+//currencyConverter
+
+const crnbtn = document.getElementById('crn-btn')
+const output = document.getElementById('output')
+
+//marrigeSatus
+const maName = document.getElementById('ma_name')
+const maAge = document.getElementById('ma_age')
+
+const maButton = document.getElementById('ma_button')
+const maOutput = document.getElementById('ma_output')
+
+
+//simpleFormValidation
+password.onkeyup = () => {
+  
+    if(password.value.length < 6){
+        input_box[2].children[4].innerHTML = `<b style="color:red;">Your Password must be at least 6 characters.</b>`
+        password.style.border = '1px solid red';
+
+    }else if(password.value.search(/[a-z]/i) < 0){
+        input_box[2].children[4].innerHTML = `<b style="color:red;">Must contain at least one Letter.</b>` 
+    }else if(password.value.search(/[@!#$%&\*]/) < 0){
+        input_box[2].children[4].innerHTML = `<b style="color:red;">Must contain at least one Speacial Character.</b>`   
+    }else if(password.value.search(/[0-9]/) < 0){
+        input_box[2].children[4].innerHTML = `<b style="color:orange;">Must contain at least one Digit.</b>`   
+    }else{
+        input_box[2].children[4].innerHTML = `<b style="color:green;">Your Password Is Strong.</b>` 
+        password.style.border = '1px solid green'
+    }
+}
+
+password.onblur = ()  => {
+    input_box[2].children[4].innerHTML = ''
+}
+
+//nameValidation
+name.addEventListener('keyup', function(){
+    if((/^[a-zA-Z0-9 ]*$/).test(name.value)){
+        name.style.border =  '1px solid green'
+    }else if(name.value == ''){
+        name.style.border =  '1px solid red'
+    }else{
+        name.style.border =  '1px solid red'
+    }
+})
+
+name.addEventListener('blur', function(){
+    if(name.value == ''){
+        name.style.border =  '1px solid red'
+    }
+})
+
+
+//mailvalidation
+email.addEventListener('keyup', function(){
+    if((/^[a-z0-9_\.]*@[a-z]{1,6}\.[a-z]{2,7}$/).test(email.value)){
+        email.style.border =  '1px solid green'
+    }else{
+        email.style.border =  '1px solid red'
+    }
+})
+
+//phoneValidation
+phone.addEventListener('keyup', () => {
+     if((/^(01|8801|\+8801)[0-9]{9}$/).test(phone.value)){
+        phone.style.border = '1px solid green';  
+     }else{
+        phone.style.border = '1px solid red ';   
+     }
+
+})
+
+phone.addEventListener('blur', () => {
+    if((/^(01|8801|\+8801)[0-9]{9}$/).test(phone.value)){
+       phone.style.border = '1px solid green';  
+    }else{
+       phone.style.border = '1px solid red ';   
+    }
+
+})
+
+
+zipCode.onkeyup = () => {
+     if((/^[0-9]{4,}$/).test(zipCode.value)){
+        zipCode.style.border = '1px solid green'
+     }else{
+        zipCode.style.border = '1px solid red'
+     }
+}
+
+
+
+
+
+city.onclick = () => {
+     if(city.value != ''){
+        city.style.border = '1px solid green'
+     }
+}
+
+//simpleFormPreventDefault
+simpleForm.onsubmit = (e) => {
+    e.preventDefault()
+    
+     name.style.border = name.value == '' ? '1px solid red' : '1px solid green'
+     email.style.border = email.value == '' ? '1px solid red' : '1px solid green'
+     password.style.border = password.value == '' ? '1px solid red' : '1px solid green';
+     phone.style.border = phone.value == ''  ? '1px solid red' : '1px solid green';
+     city.style.border = city.value == ''  ? '1px solid red' : '1px solid green';
+     zipCode.style.border = zipCode.value == '' || zipCode.value.length < 4  ? '1px solid red' : '1px solid green';
+
+
+     if(name.value == '' || email.value == '' || password.value == '' || phone.value == '' || city.value == '' || zipCode.value == '' || zipCode.value.length < 4){
+          alrt.innerHTML = `<b style="color:red;">All Feilds Are Required!</b>` 
+     }else if(((/^[a-zA-Z0-9 ]*$/).test(name.value) != true )|| (password.value.length < 6) || ((/^[a-z0-9_\.]*@[a-z]{1,6}\.[a-z]{2,7}$/).test(email.value) != true )||( (/^(01|8801|\+8801)[0-9]{9}$/).test(phone.value) != true)){
+        alrt.innerHTML = `<b style="color:red;">All Feilds Must be Fill With Valid information!</b>` ;
+       
+     }else{
+        alrt.innerHTML = `<b style="color:#fff;background:#45b78d;">Data is Stable.</b>`  
+     }
+       
+
+}
+
+
+//currencyConverter
+crnbtn.onclick = () => {
+    const bdtamount = document.getElementById('bdtamount').value
+    const crnlist = document.getElementById('crnlist').value
+     
+    if(bdtamount == ''){
+        output.innerHTML = `
+           <p style="color:#ecf0f1;">Please Input Your amount.</p>
+       `
+    }else if(crnlist == ''){
+        output.innerHTML =`
+             <p style="color:#ecf0f1;">Please Select a Currency Type</p>
+          `
+    }else{
+        output.innerHTML = `
+             <p>${currencyc(bdtamount,crnlist)}</p> 
+        `
+    }
+    
+}
+
+//marriageStatus
+
+
+maAge.onkeyup = () => {
+    if(maAge.value.length < 4 || maAge.value > new Date().getFullYear()){
+        maAge.style.border = '1px solid red'
+    }else{
+        maAge.style.border = '1px solid green'
+    }
+}
+
+maButton.addEventListener('click', () => {
+    const maGender = document.getElementById('ma_gender')
+     maName.style.border = (maName.value == '') ? '1px solid red': '1px solid green'  ;
+     maAge.style.border = (maAge.value == '') ? '1px solid red' :'1px solid green' ;
+     maGender.style.border = (maGender.value == '') ? '1px solid red' :'1px solid green' ;
+       
+     if(maName.value == '' || maAge.value == '' || maGender == '' || maAge.value.length < 4 || maAge.value > new Date().getFullYear()){
+         maOutput.innerHTML = `
+                    <p style="color:#d35400;">All Feild Required!</p>
+         ` 
+         maAge.style.border = '1px solid red'
+     }else{
+        maOutput.innerHTML = `
+              ${marriage(maName.value,maAge.value,maGender.value)}
+
+        `
+
+     }
+})
+
+ 
+
+
+
+
+
+
+
+
+
---
+++ function.js
@@ -0,0 +1,116 @@
+/**
+ * 
+ * @param {*} input 
+ * @param {*} fromm 
+ * @returns 
+ */
+function currencyc(input,fromm) {
+      if(fromm == 'usd'){
+         return `${input} $ =  ${(input * 85.74).toFixed(2)} BDT`
+      }else if(fromm == 'cad'){
+         return `${input} C$ = ${(input * 67.93).toFixed(2)} BDT`
+      }else if(fromm == 'euro'){
+         return `${input} € = ${(input * 97.52).toFixed(2)} BDT`
+      }else if(fromm == 'pound'){
+         return `${input} £ = ${(input * 116.02).toFixed(2)} BDT`
+      }
+
+}
+
+//marriageChecker
+
+let marriage = (name,year,gender) => {
+       let date = new Date()
+       let age = date.getFullYear() - year
+
+       return `
+             <p class="text-${marriageSatus(age,gender).clsss}">Hi ${name} You are ${age} ${age > 1 ? 'Years' : 'Year'} Old. ${marriageSatus(age,gender).agesta} </p>
+       `   
+}
+
+
+function marriageSatus(year,gender){
+       if(year >= 0 && year <= 10){
+                     if(gender == 'male'){
+                           return {
+                                 agesta : `Tumi Ekhno Babu acho. tmr biyer boyos hote   ekhno ${(22 - year)} ${(22 - year) > 1 ? 'Years' : 'Year'} Baki`,
+                                 clsss  :'danger'
+                           }
+                     }else if(gender == 'female'){
+                              return {
+                                 agesta : `Tumi Ekhno Baby acho. tmr biyer boyos hote   ekhno ${(18 - year)} ${(18 - year) > 1 ? 'Years' : 'Year'} Baki`,
+                                 clsss  :'danger'
+                           }
+                     }
+  
+         }else if(year >= 11 && year <= 17){
+                  if(gender == 'male'){
+                        return {
+                              agesta : `Tumi Ekhn Kishor. tmr biyer boyos   hote ekhno ${(22 - year)} ${(22 - year) > 1 ? 'Years' : 'Year'} Baki`,
+                              clsss  :'warning'
+                        }
+                  }else if(gender == 'female'){
+                           return {
+                              agesta : `Tumi Ekhn kishori. tmr biyer boyos hote  ekhno ${(18 - year)} ${(18 - year) > 1 ? 'Years' : 'Year'} Baki`,
+                              clsss  :'warning'
+                        }
+                  }
+
+         }else if(year >= 18 && year <= 30){
+                  if(gender == 'male'){
+                          if(year <= 21){
+                              return {
+                                    agesta : `Tumi Ekhn Kishor. tmr biyer boyos   hote ekhno ${(22 - year)} ${(22 - year) > 1 ? 'Years' : 'Year'} Baki`,
+                                    clsss  :'warning'
+                                }
+                          }else{
+                              return {
+                                    agesta : `Congratulations!  Young Man etai tmr biyer asol boyos.Jao patri dekha start koro ekhni.`,
+                                    clsss  :'success'
+                                }
+                          }
+                  }else if(gender == 'female'){
+                           return {
+                              agesta : `Tumi Ekhn Juboti.Congratulations!  Young Lady etai tmr biyer asol boyos.cheleder proposal Pele raji hoye jao ekhni.`,
+                              clsss  :'success'
+                        }
+                  }
+
+         }else if(year >= 31 && year <= 40){
+                     
+                     if(gender == 'male'){
+                           return {
+                                 agesta : `Amr mone hocche tmr biye korate asolei Joruri. r late kora moteo thik hbe na`,
+                                 clsss  :'info'
+                           }
+                     }else if(gender == 'female'){
+                              return {
+                                 agesta : `Amr mone hocche apnar biye korate asolei Joruri. r late kora moteo thik hbe na`,
+                                 clsss  :'info'
+                           }
+                     }
+                  
+
+
+         }else if(year >= 41 ){
+                  if(gender == 'male'){
+                        return {
+                              agesta : `Apni ekhno biye koren ni...!`,
+                              clsss  :'warning'
+                        }
+                  }else if(gender == 'female'){
+                           return {
+                              agesta : `Apni ekhno biye koren ni...!`,
+                              clsss  :'warning'
+                        }
+                  }
+
+         }
+
+
+
+
+}
+
+
+
